5b5d92d97f5ddf8c0ae6dd2db10639e7c6b9fd67
大石头 <nnhy@vip.qq.com>
2019-02-06 11:01:44 +0800

    netcore项目既可以独立运行，也可以作为其它项目的类库，便于二次开发扩展

--- Test/Program.cs
+++ Test/Program.cs
@@ -1,12 +1,49 @@
 ﻿using System;
+using System.Net;
+using System.Threading;
+using NewLife.Log;
+using NewLife.Net;
+using NewLife.Remoting;
+using Stardust;
+using Setting = Stardust.Setting;
 
 namespace Test
 {
     class Program
     {
-        static void Main(string[] args)
+        static ApiServer _Server;
+        static void Main(String[] args)
         {
-            Console.WriteLine("Hello World!");
+            XTrace.UseConsole();
+
+            var set = Setting.Current;
+
+            var sc = new ApiServer(set.Port)
+            {
+                Log = XTrace.Log
+            };
+            if (set.Debug)
+            {
+                var ns = sc.EnsureCreate() as NetServer;
+                ns.Log = XTrace.Log;
+#if DEBUG
+                ns.LogSend = true;
+                ns.LogReceive = true;
+                sc.EncoderLog = XTrace.Log;
+#endif
+            }
+
+            // 注册服务
+            sc.Register<StarService>();
+
+            StarService.Log = XTrace.Log;
+            StarService.Local = new IPEndPoint(NetHelper.MyIP(), set.Port);
+
+            sc.Start();
+
+            _Server = sc;
+
+            Thread.Sleep(-1);
         }
     }
 }
