7174fee856869218d445357ab530da4725d5e020
大石头 <nnhy@vip.qq.com>
2019-05-26 15:15:43 +0800

    星尘，分布式资源调度，客户端代理部署于每一台机器节点，接受服务端命令，获取目标应用包并拉起进程。

---
+++ StarAgent/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace StarClient
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
