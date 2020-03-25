020bb423f3ed31e8783c35572ec49083718e068d
大石头 <nnhy@vip.qq.com>
2020-03-25 23:47:48 +0800

    尝试 WorkerService 托管星尘代理

---
+++ StarAgent3/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.IO;
+using System.Reflection;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using NewLife;
+using NewLife.Log;
+
+namespace StarAgent3
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            XTrace.UseConsole();
+
+            XTrace.WriteLine("FullPath:{0}", ".".GetFullPath());
+            XTrace.WriteLine("BasePath:{0}", ".".GetBasePath());
+            XTrace.WriteLine("TempPath:{0}", Path.GetTempPath());
+
+            var mi = MachineInfo.Current ?? MachineInfo.RegisterAsync().Result;
+
+            foreach (var pi in mi.GetType().GetProperties(BindingFlags.Public | BindingFlags.Instance))
+            {
+                XTrace.WriteLine("{0}:\t{1}", pi.Name, mi.GetValue(pi));
+            }
+
+            CreateHostBuilder(args).Build().Run();
+        }
+
+        public static IHostBuilder CreateHostBuilder(string[] args) =>
+            Host.CreateDefaultBuilder(args)
+                .ConfigureServices((hostContext, services) =>
+                {
+                    services.AddHostedService<Worker>();
+                });
+    }
+}
---
+++ StarAgent3/Worker.cs
@@ -0,0 +1,121 @@
+﻿using System;
+using System.Diagnostics;
+using System.Threading;
+using System.Threading.Tasks;
+using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
+using NewLife;
+using NewLife.Log;
+using NewLife.Threading;
+using Stardust;
+
+namespace StarAgent3
+{
+    public class Worker : BackgroundService
+    {
+        private readonly ILogger<Worker> _logger;
+
+        public Worker(ILogger<Worker> logger)
+        {
+            _logger = logger;
+        }
+
+        public override Task StartAsync(CancellationToken cancellationToken)
+        {
+            XTrace.WriteLine("StartAsync");
+
+            return base.StartAsync(cancellationToken);
+        }
+
+        public override Task StopAsync(CancellationToken cancellationToken)
+        {
+            XTrace.WriteLine("StopAsync");
+
+            return base.StopAsync(cancellationToken);
+        }
+
+        protected override async Task ExecuteAsync(CancellationToken stoppingToken)
+        {
+            var set = StarAgent.Setting.Current;
+
+            StartClient();
+
+            // 应用服务管理
+            _Manager = new ServiceManager
+            {
+                Services = set.Services,
+
+                Log = XTrace.Log,
+            };
+            _Manager.Start();
+        }
+
+        private static TimerX _timer;
+        private static StarClient _Client;
+        private static ServiceManager _Manager;
+        private static void StartClient()
+        {
+            var set = StarAgent.Setting.Current;
+            var server = set.Server;
+            if (server.IsNullOrEmpty()) return;
+
+            XTrace.WriteLine("初始化服务端地址：{0}", server);
+
+            var client = new StarClient(server)
+            {
+                Code = set.Code,
+                Secret = set.Secret,
+                Log = XTrace.Log,
+            };
+
+            // 登录后保存证书
+            client.OnLogined += (s, e) =>
+            {
+                var inf = client.Info;
+                if (inf != null && !inf.Code.IsNullOrEmpty())
+                {
+                    set.Code = inf.Code;
+                    set.Secret = inf.Secret;
+                    set.Save();
+                }
+            };
+
+            // 可能需要多次尝试
+            _timer = new TimerX(TryConnectServer, client, 0, 5_000) { Async = true };
+
+            _Client = client;
+        }
+
+        private static void TryConnectServer(Object state)
+        {
+            var client = state as StarClient;
+            var set = StarAgent.Setting.Current;
+            //Task.Run(client.Login).ContinueWith(t => CheckUpgrade(client, set.Channel));
+            client.Login().Wait();
+            CheckUpgrade(client, set.Channel);
+
+            // 登录成功，销毁定时器
+            //TimerX.Current.Period = 0;
+            _timer.TryDispose();
+            _timer = null;
+        }
+
+        private static void CheckUpgrade(StarClient client, String channel)
+        {
+            // 检查更新
+            var ur = client.Upgrade(channel).Result;
+            if (ur != null)
+            {
+                var rs = client.ProcessUpgrade(ur);
+
+                // 强制更新时，马上重启
+                if (rs && ur.Force)
+                {
+                    var p = Process.GetCurrentProcess();
+                    p.Close();
+                    p.Kill(true);
+                }
+            }
+        }
+    }
+}
