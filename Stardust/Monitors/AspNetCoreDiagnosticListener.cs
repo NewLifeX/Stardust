1fe02bf919fa0afecfcb166c0597bc929e84e89a
大石头 <nnhy@vip.qq.com>
2020-07-27 15:22:51 +0800

    AspNetCore跟踪监听器强依赖asp.netcore，不如中间件实用

--- Stardust/Monitors/AspNetCoreDiagnosticListener.cs
+++ Stardust/Monitors/AspNetCoreDiagnosticListener.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿#if __CORE__
+using System;
 using System.Collections.Generic;
 using System.Diagnostics;
 using System.Net.Http;
@@ -8,28 +9,22 @@ using NewLife.Reflection;
 
 namespace Stardust.Monitors
 {
-    /// <summary>Http诊断监听器</summary>
-    public class HttpDiagnosticListener : TraceDiagnosticListener
+    /// <summary>ASP.NETCore诊断监听器</summary>
+    public class AspNetCoreDiagnosticListener : TraceDiagnosticListener
     {
         /// <summary>实例化</summary>
-        public HttpDiagnosticListener()
+        public AspNetCoreDiagnosticListener()
         {
-            Name = "HttpHandlerDiagnosticListener";
+            Name = "Microsoft.AspNetCore";
         }
 
         /// <summary>下一步</summary>
         /// <param name="value"></param>
         public override void OnNext(KeyValuePair<String, Object> value)
         {
-            //base.OnNext(value);
-
-#if DEBUG
-            XTrace.WriteLine(value.Key);
-#endif
-
             switch (value.Key)
             {
-                case "System.Net.Http.HttpRequestOut.Start":
+                case "Microsoft.AspNetCore.Hosting.BeginRequest":
                     {
                         if (value.Value.GetValue("Request") is HttpRequestMessage request)
                         {
@@ -38,7 +33,7 @@ namespace Stardust.Monitors
 
                         break;
                     }
-                case "System.Net.Http.Exception":
+                case "Microsoft.AspNetCore.Hosting.UnhandledException":
                     {
                         var span = DefaultSpan.Current;
                         if (span != null && value.Value.GetValue("Exception") is Exception ex)
@@ -48,21 +43,37 @@ namespace Stardust.Monitors
                         break;
                     }
 
-                case "System.Net.Http.HttpRequestOut.Stop":
+                case "Microsoft.AspNetCore.Hosting.EndRequest":
                     {
                         var span = DefaultSpan.Current;
                         if (span != null)
                         {
-                            if (value.Value.GetValue("Response") is HttpResponseMessage response && !response.IsSuccessStatusCode)
-                            {
-                                if (span.Error.IsNullOrEmpty()) span.Error = response.ReasonPhrase;
-                            }
-
                             span.Dispose();
                         }
                         break;
                     }
             }
         }
+
+        /// <summary>忽略的后缀</summary>
+        public static String[] ExcludeSuffixes { get; set; } = new[] {
+            ".html", ".htm", ".js", ".css", ".map", ".png", ".jpg", ".gif", ".ico",  // 脚本样式图片
+            ".woff", ".woff2", ".svg", ".ttf", ".otf", ".eot"   // 字体
+        };
+
+        private static String GetAction(HttpContext ctx)
+        {
+            var p = ctx.Request.Path + "";
+            if (p.EndsWithIgnoreCase(ExcludeSuffixes)) return null;
+
+            var ss = p.Split('/');
+            if (ss.Length == 0) return p;
+
+            // 如果最后一段是数字，则可能是参数，需要去掉
+            if ((ss.Length == 4 || ss.Length == 5) && ss[ss.Length - 1].ToInt() > 0) p = "/" + ss.Take(ss.Length - 1).Join("/");
+
+            return p;
+        }
     }
 }
+#endif
