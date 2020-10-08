065cd335afb03557ea19acd60bfbbff5e8bc40ef
大石头 <nnhy@vip.qq.com>
2020-10-08 10:26:58 +0800

    星尘网关

---
+++ StarGateway/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Threading.Tasks;
+using Microsoft.Extensions.Hosting;
+using NewLife.Log;
+using Stardust.Data;
+
+namespace StarGateway
+{
+    class Program
+    {
+        public static void Main(String[] args)
+        {
+            XTrace.UseConsole();
+
+            // 异步初始化
+            Task.Run(InitAsync);
+
+            CreateWebHostBuilder(args).Build().Run();
+        }
+
+        public static IHostBuilder CreateWebHostBuilder(String[] args)
+        {
+            var builder = Host.CreateDefaultBuilder(args);
+            //builder.ConfigureWebHostDefaults(webBuilder =>
+            //{
+            //    var set = Setting.Current;
+            //    if (set.Port > 0) webBuilder.UseUrls($"http://*:{set.Port}");
+            //    webBuilder.UseStartup<Startup>();
+            //});
+
+            return builder;
+        }
+
+        private static void InitAsync()
+        {
+            // 配置
+            var set = NewLife.Setting.Current;
+            if (set.IsNew)
+            {
+                set.DataPath = "../Data";
+                set.Save();
+            }
+
+            // 初始化数据库
+            var n = App.Meta.Count;
+            AppStat.Meta.Session.Dal.Db.ShowSQL = false;
+        }
+    }
+}
---
+++ StarGateway/Setting.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.ComponentModel;
+using NewLife.Configuration;
+
+namespace StarGateway
+{
+    /// <summary>配置</summary>
+    [Config("StarGateway")]
+    public class Setting : Config<Setting>
+    {
+        #region 属性
+        /// <summary>调试开关。默认true</summary>
+        [Description("调试开关。默认true")]
+        public Boolean Debug { get; set; } = true;
+
+        /// <summary>服务端口。默认8800</summary>
+        [Description("服务端口。默认8800")]
+        public Int32 Port { get; set; } = 8800;
+
+        /// <summary>令牌密钥。用于生成JWT令牌的算法和密钥，如HS256:ABCD1234</summary>
+        [Description("令牌密钥。用于生成JWT令牌的算法和密钥，如HS256:ABCD1234")]
+        public String TokenSecret { get; set; }
+        #endregion
+    }
+}
