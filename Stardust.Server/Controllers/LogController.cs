0b61c489511ad3c527359be1fbdd049962298d18
智能石头 <nnhy@vip.qq.com>
2020-10-28 16:42:53 +0800

    调整解析算法，前向探索式

--- Stardust.Server/Controllers/LogController.cs
+++ Stardust.Server/Controllers/LogController.cs
@@ -1,8 +1,7 @@
 ﻿using System;
-using System.IO;
-using System.Text;
 using Microsoft.AspNetCore.Mvc;
 using NewLife;
+using NewLife.Collections;
 using Stardust.Data;
 
 namespace Stardust.Server.Controllers
@@ -43,37 +42,33 @@ namespace Stardust.Server.Controllers
                 if (app != null && app.Enable)
                 {
                     // 00:00:04.205  7 Y 1 NewLife.Core v8.10.2020.1020
-                    using var reader = new StringReader(content);
-                    var sb = new StringBuilder();
-                    var ss = new String[4];
-                    while (true)
+                    var lines = content.Split(Environment.NewLine);
+                    for (var i = 0; i < lines.Length; i++)
                     {
-                        var line = reader.ReadLine();
-                        if (line == null) break;
-
                         // 时间、线程、类型、名称
-                        var ss2 = new String[4];
-                        var p = ReadExpect(line, ' ', ss2);
-                        if (p < 0) continue;
-
-                        // 发现新行，保存
-                        if (sb.Length > 0 && ss2[0] != null && ss2[0].Length == 12 && ss2[1].ToInt() > 0)
+                        var ss = new String[4];
+                        var p = ReadExpect(lines[i], ' ', ss);
+                        if (p > 0 && ss[0] != null && ss[0].Length == 12 && ss[1].ToInt() > 0)
                         {
-                            AppLog.Create(app.ID, clientId, ss, sb.ToString(), ip);
+                            var msg = lines[i].Substring(p)?.Trim();
+                            var sb = Pool.StringBuilder.Get();
+                            sb.AppendLine(msg);
+
+                            // 尝试后续行
+                            for (var j = i + 1; j < lines.Length; j++)
+                            {
+                                var ss2 = new String[4];
+                                var p2 = ReadExpect(lines[j], ' ', ss2);
+                                if (p2 > 0 && ss2[0] != null && ss2[0].Length == 12 && ss2[1].ToInt() > 0) break;
+
+                                sb.AppendLine(lines[j]);
+                                i++;
+                            }
+                            msg = sb.Put(true);
+
+                            AppLog.Create(app.ID, clientId, ss, msg, ip);
                             sb.Clear();
-
-                            ss = ss2;
                         }
-
-                        if (sb.Length > 0) sb.AppendLine();
-                        sb.AppendLine(line.Substring(p)?.Trim());
-                    }
-
-                    // 残留
-                    if (sb.Length > 0)
-                    {
-                        AppLog.Create(app.ID, clientId, ss, sb.ToString(), ip);
-                        sb.Clear();
                     }
                 }
             }
