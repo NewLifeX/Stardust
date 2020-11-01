aa22d49c9ce86e7fe86f970457e21c3b4f23c6bd
大石头 <nnhy@vip.qq.com>
2020-11-01 08:43:11 +0800

    单元测试

--- Stardust.ServerTests/Controllers/OAuthControllerTests.cs
+++ Stardust.ServerTests/Controllers/OAuthControllerTests.cs
@@ -32,6 +32,46 @@ namespace Stardust.Server.Controllers.Tests
             var rs = await client.GetAsync<TokenModel>("oauth/token", model);
             Assert.NotNull(rs);
             Assert.NotEmpty(rs.AccessToken);
+            Assert.NotEmpty(rs.RefreshToken);
+            Assert.Equal(7200, rs.ExpireIn);
+            Assert.Equal("JWT", rs.TokenType);
+        }
+
+        [Fact]
+        public async void Token_refresh_token()
+        {
+            var client = _server.CreateClient();
+
+            var refresh_token = "";
+            {
+                var model = new TokenInModel
+                {
+                    grant_type = "refresh_token",
+                    UserName = "stone",
+                };
+
+                var rs = await client.GetAsync<TokenModel>("oauth/token", model);
+                Assert.NotNull(rs);
+                Assert.NotEmpty(rs.RefreshToken);
+
+                refresh_token = rs.RefreshToken;
+            }
+
+            // 刷新令牌
+            {
+                var model2 = new TokenInModel
+                {
+                    grant_type = "refresh_token",
+                    refresh_token = refresh_token,
+                };
+
+                var rs2 = await client.GetAsync<TokenModel>("oauth/token", model2);
+                Assert.NotNull(rs2);
+                Assert.NotEmpty(rs2.AccessToken);
+                Assert.NotEmpty(rs2.RefreshToken);
+                Assert.Equal(7200, rs2.ExpireIn);
+                Assert.Equal("JWT", rs2.TokenType);
+            }
         }
     }
 }
