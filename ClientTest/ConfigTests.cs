c7d8ca6ff359d4554642ecda3b8f1c5b7dc2371e
智能石头 <nnhy@vip.qq.com>
2021-01-20 11:33:00 +0800

    配置接口单元测试通过

---
+++ ClientTest/ConfigTests.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NewLife.Configuration;
+using Xunit;
+
+namespace ClientTest
+{
+    public class ConfigTests
+    {
+        [Fact]
+        public void Http_Test()
+        {
+            {
+                var prv = new HttpConfigProvider
+                {
+                    Server = "http://127.0.0.1:6600",
+                    AppId = "StarWeb"
+                };
+
+                var title = prv["Title"];
+                Assert.Equal("NewLife开发团队", title);
+
+                var shop = prv["conn_shop"];
+                Assert.Equal("server=10.0.0.1;user=maindb;pass=Pass@word", shop);
+            }
+            {
+                var prv = new HttpConfigProvider
+                {
+                    Server = "http://127.0.0.1:6600",
+                    AppId = "StarWeb",
+                    Scope = "dev",
+                };
+
+                var title = prv["Title"];
+                Assert.Equal("NewLife开发团队", title);
+
+                var shop = prv["conn_shop"];
+                Assert.Equal("server=192.168.0.1;user=dev;pass=dev1234", shop);
+            }
+        }
+    }
+}
