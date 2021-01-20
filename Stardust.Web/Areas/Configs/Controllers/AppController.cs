4917303a50afb7356ea6731a85f7d12da4f17ff0
智能石头 <nnhy@vip.qq.com>
2021-01-20 10:29:36 +0800

    应用配置入口页面

---
+++ Stardust.Web/Areas/Configs/Controllers/AppController.cs
@@ -0,0 +1,31 @@
+﻿using NewLife.Cube;
+using Stardust.Data;
+
+namespace Stardust.Web.Areas.Configs.Controllers
+{
+    [ConfigsArea]
+    public class AppController : EntityController<App>
+    {
+        static AppController()
+        {
+            MenuOrder = 58;
+
+            {
+                var df = ListFields.AddDataField("Configs", "Enable");
+                df.Header = "配置";
+                df.DisplayName = "配置";
+                df.Url = "ConfigData?appId={ID}";
+            }
+        }
+
+        //protected override IEnumerable<AppConfig> Search(Pager p)
+        //{
+        //    var appId = p["appId"].ToInt(-1);
+
+        //    var start = p["dtStart"].ToDateTime();
+        //    var end = p["dtEnd"].ToDateTime();
+
+        //    return AppConfig.Search(appId, start, end, p["Q"], p);
+        //}
+    }
+}
