e3cc114aae168ad377b8d012cbf84f4be0cf63e9
大石头 <nnhy@vip.qq.com>
2021-03-20 09:10:07 +0800

    调整Redis节点页面

--- Stardust.Web/Areas/Redis/Controllers/RedisNodeController.cs
+++ Stardust.Web/Areas/Redis/Controllers/RedisNodeController.cs
@@ -25,6 +25,29 @@ namespace Stardust.Web.Areas.Redis.Controllers
         {
             MenuOrder = 50;
 
+            ListFields.RemoveField("WebHook", "AlarmMemoryRate", "AlarmConnections", "AlarmSpeed", "AlarmInputKbps", "AlarmOutputKbps");
+            ListFields.RemoveCreateField();
+            ListFields.RemoveField("UpdateUser", "UpdateUserID", "UpdateIP", "Remark");
+
+            {
+                var df = ListFields.AddDataField("Monitor", "UpdateTime");
+                df.DisplayName = "监控";
+                df.Header = "监控";
+                df.Url = "RedisData?redisId={Id}";
+            }
+            {
+                var df = ListFields.AddDataField("Queue", "UpdateTime");
+                df.DisplayName = "队列";
+                df.Header = "队列";
+                df.Url = "RedisMessageQueue?redisId={Id}";
+            }
+            {
+                var df = ListFields.AddDataField("Refresh", "UpdateTime");
+                df.DisplayName = "刷新";
+                df.Header = "刷新";
+                df.Url = "RedisNode/Refresh?Id={Id}";
+                df.DataAction = "action";
+            }
             {
                 var df = ListFields.AddDataField("Log", "UpdateTime");
                 df.DisplayName = "修改日志";
