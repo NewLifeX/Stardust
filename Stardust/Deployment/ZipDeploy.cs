b3f62a384963f7b6cbb7749be0b4968b3a31e8e7
大石头 <nnhy@vip.qq.com>
2022-08-07 11:43:46 +0800

    设置正确的基础目录和工作目录

--- Stardust/Deployment/ZipDeploy.cs
+++ Stardust/Deployment/ZipDeploy.cs
@@ -71,14 +71,15 @@ public class ZipDeploy
         var fi = FileName?.AsFile();
         if (fi == null || !fi.Exists) throw new Exception("未指定Zip文件");
 
-        if (Name.IsNullOrEmpty()) Name = Path.GetFileNameWithoutExtension(FileName);
+        var name = Name;
+        if (name.IsNullOrEmpty()) name = Name = Path.GetFileNameWithoutExtension(FileName);
 
         var hash = fi.MD5().ToHex()[..8].ToLower();
         var rundir = fi.Directory;
         var shadow = Shadow;
-        if (shadow.IsNullOrEmpty()) shadow = Path.GetTempPath().CombinePath(Name);
+        if (shadow.IsNullOrEmpty()) shadow = Shadow = Path.GetTempPath().CombinePath(name);
 
-        WriteLog("ZipDeploy {0}", Name);
+        WriteLog("ZipDeploy {0}", name);
         WriteLog("运行目录 {0}", rundir);
 
         // 影子目录，用于解压缩应用
@@ -86,31 +87,88 @@ public class ZipDeploy
         if (!Path.IsPathRooted(shadow)) shadow = rundir.FullName.CombinePath(shadow).GetFullPath();
         WriteLog("影子目录 {0}", shadow);
 
-        if (!Directory.Exists(shadow))
-        {
-            WriteLog("解压缩 {0}", FileName);
-
-            fi.Extract(shadow, true);
-
-            // 复制配置文件和数据文件到运行目录
-            foreach (var item in shadow.AsDirectory().GetFiles())
-            {
-                if (item.Extension.EndsWithIgnoreCase(".json", ".config", ".xml"))
-                {
-                    item.CopyTo(rundir.FullName.CombinePath(item.Name), true);
-                }
-            }
-
-            var di = shadow.CombinePath("Config").AsDirectory();
-            if (di.Exists) di.CopyTo(rundir.FullName.CombinePath("Config"));
-
-            di = shadow.CombinePath("Data").AsDirectory();
-            if (di.Exists) di.CopyTo(rundir.FullName.CombinePath("Data"));
-        }
+        if (!Directory.Exists(shadow)) Extract(shadow);
 
         // 查找可执行文件
+        var runfile = FindExeFile(shadow);
+
+        // 执行
+        if (runfile.IsNullOrEmpty())
+        {
+            WriteLog("无法找到名为[{0}]的可执行文件", name);
+            return;
+        }
+
+        // 如果带有 NewLife.Core.dll ，重定向基础目录
+        //Arguments = $"{Arguments} --BasePath={rundir}".Trim();
+        Environment.SetEnvironmentVariable("BasePath", rundir.FullName);
+
+        WriteLog("执行 {0}", runfile);
+
+        ProcessStartInfo si = null;
+        if (rundir.Extension.EqualIgnoreCase(".dll"))
+        {
+            si = new ProcessStartInfo
+            {
+                FileName = "dotnet",
+                Arguments = $"{FileName} {Arguments}",
+                WorkingDirectory = rundir.FullName,
+                UseShellExecute = false,
+            };
+        }
+        else
+        {
+            si = new ProcessStartInfo
+            {
+                FileName = runfile,
+                Arguments = Arguments,
+                WorkingDirectory = rundir.FullName,
+                UseShellExecute = false,
+            };
+        }
+
+        WriteLog("启动文件: {0}", si.FileName);
+        WriteLog("启动参数: {0}", si.Arguments);
+
+        var p = Process.Start(si);
+        if (p.WaitForExit(3_000))
+            WriteLog("启动失败！ExitCode={0}", p.ExitCode);
+        else
+            WriteLog("启动成功！");
+    }
+
+    /// <summary>解压缩</summary>
+    /// <param name="shadow"></param>
+    protected virtual void Extract(String shadow)
+    {
+        var fi = FileName.AsFile();
+        var rundir = fi.Directory;
+        WriteLog("解压缩 {0}", FileName);
+
+        fi.Extract(shadow, true);
+
+        // 复制配置文件和数据文件到运行目录
+        foreach (var item in shadow.AsDirectory().GetFiles())
+        {
+            if (item.Extension.EndsWithIgnoreCase(".json", ".config", ".xml"))
+            {
+                item.CopyTo(rundir.FullName.CombinePath(item.Name), true);
+            }
+        }
+
+        var di = shadow.CombinePath("Config").AsDirectory();
+        if (di.Exists) di.CopyTo(rundir.FullName.CombinePath("Config"));
+
+        di = shadow.CombinePath("Data").AsDirectory();
+        if (di.Exists) di.CopyTo(rundir.FullName.CombinePath("Data"));
+    }
+
+    /// <summary>查找执行文件</summary>
+    /// <param name="shadow"></param>
+    /// <returns></returns>
+    protected virtual String FindExeFile(String shadow)
+    {
         var fis = shadow.AsDirectory().GetFiles();
-        //WriteLog("可用文件：{0}", fis.Join(",", e => e.Name));
         var runfile = fis.FirstOrDefault(e => e.Name.EqualIgnoreCase(Name));
         if (runfile == null && Runtime.Windows)
         {
@@ -124,57 +182,27 @@ public class ZipDeploy
                 if (exes.Count == 1) runfile = exes[0];
             }
         }
+
+        // 跟配置文件配套的dll
+        if (runfile == null)
+        {
+            var ext = $".runtimeconfig.json";
+            var cfg = fis.FirstOrDefault(e => e.Name.EndsWithIgnoreCase(ext));
+            if (cfg != null)
+            {
+                var name = $"{cfg.Name[..^ext.Length]}.dll";
+                runfile = fis.FirstOrDefault(e => e.Name.EqualIgnoreCase(name));
+            }
+        }
+
+        // 指定名称dll
         if (runfile == null)
         {
             var name = $"{Name}.dll";
             runfile = fis.FirstOrDefault(e => e.Name.EqualIgnoreCase(name));
         }
 
-        // 如果带有 NewLife.Core.dll ，重定向基础目录
-        if (fis.Any(e => e.Name.EqualIgnoreCase("NewLife.Core.dll")))
-        {
-            //Arguments = $"{Arguments} --BasePath={rundir}".Trim();
-            Environment.SetEnvironmentVariable("BasePath", rundir.FullName);
-        }
-
-        // 执行
-        if (runfile == null)
-        {
-            WriteLog("无法找到名为[{0}]的可执行文件", Name);
-        }
-        else
-        {
-            WriteLog("执行 {0}", runfile);
-
-            ProcessStartInfo si = null;
-            if (rundir.Extension.EqualIgnoreCase(".dll"))
-            {
-                si = new ProcessStartInfo
-                {
-                    FileName = "dotnet",
-                    Arguments = $"{FileName} {Arguments}",
-                    UseShellExecute = false,
-                };
-            }
-            else
-            {
-                si = new ProcessStartInfo
-                {
-                    FileName = runfile.FullName,
-                    Arguments = Arguments,
-                    UseShellExecute = false,
-                };
-            }
-
-            WriteLog("启动文件: {0}", si.FileName);
-            WriteLog("启动参数: {0}", si.Arguments);
-
-            var p = Process.Start(si);
-            if (p.WaitForExit(3_000))
-                WriteLog("启动失败！ExitCode={0}", p.ExitCode);
-            else
-                WriteLog("启动成功！");
-        }
+        return runfile?.FullName;
     }
     #endregion
 
