ff580ce8697da43a2b2d198fbbddaa7554efa068
大石头 <nnhy@vip.qq.com>
2022-08-08 18:06:49 +0800

    优化服务器信息接口

--- Stardust.Server/Controllers/ApiController.cs
+++ Stardust.Server/Controllers/ApiController.cs
@@ -1,32 +1,24 @@
-﻿using System.Reflection;
-using Microsoft.AspNetCore.Mvc;
+﻿using Microsoft.AspNetCore.Mvc;
 using NewLife.Reflection;
 using Stardust.Server.Common;
 
 namespace Stardust.Server.Controllers;
 
+[ApiFilter]
 [ApiController]
 [Route("[controller]")]
 public class ApiController : ControllerBase
 {
     private static readonly String _OS = Environment.OSVersion + "";
 
-    /// <summary>获取所有接口</summary>
-    /// <returns></returns>
-    [ApiFilter]
-    [HttpGet]
-    public Object Get() => Info(null);
-
     /// <summary>服务器信息，用户健康检测</summary>
     /// <param name="state">状态信息</param>
     /// <returns></returns>
-    [ApiFilter]
-    [HttpGet(nameof(Info))]
-    public Object Info(String state)
+    [HttpGet]
+    public Object Get(String state)
     {
         var asmx = AssemblyX.Entry;
-        var asmx2 = AssemblyX.Create(Assembly.GetExecutingAssembly());
-
+        var conn = HttpContext.Connection;
         var ip = HttpContext.GetUserHost();
 
         var rs = new
@@ -36,12 +28,12 @@ public class ApiController : ControllerBase
             asmx?.FileVersion,
             asmx?.Compile,
             OS = _OS,
-            ApiVersion = asmx2?.Version,
 
-            UserHost = HttpContext.GetUserHost(),
-            Remote = ip + "",
-            State = state,
+            UserHost = ip + "",
+            Remote = conn.RemoteIpAddress + "",
+            Port = conn.LocalPort,
             Time = DateTime.Now,
+            State = state,
         };
 
         return rs;
