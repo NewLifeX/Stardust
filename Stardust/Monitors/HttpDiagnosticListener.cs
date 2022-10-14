aec57cc0b5aac68df5f16aed04d0466a47622e05
大石头 <nnhy@vip.qq.com>
2022-10-14 14:24:02 +0800

    附加Http响应内容在原Tag信息后面

--- Stardust/Monitors/HttpDiagnosticListener.cs
+++ Stardust/Monitors/HttpDiagnosticListener.cs
@@ -1,65 +1,86 @@
 ﻿#if NET5_0_OR_GREATER
-using System;
-using System.Collections.Generic;
-using System.Diagnostics;
 using System.Net;
-using System.Net.Http;
 using NewLife;
 using NewLife.Log;
 using NewLife.Reflection;
 
-namespace Stardust.Monitors
+namespace Stardust.Monitors;
+
+/// <summary>Http诊断监听器</summary>
+public class HttpDiagnosticListener : TraceDiagnosticListener
 {
-    /// <summary>Http诊断监听器</summary>
-    public class HttpDiagnosticListener : TraceDiagnosticListener
+    private static HttpRequestOptionsKey<ISpan> _spanKey = new("Span");
+
+    /// <summary>实例化</summary>
+    public HttpDiagnosticListener() => Name = "HttpHandlerDiagnosticListener";
+
+    /// <summary>下一步</summary>
+    /// <param name="value"></param>
+    public override void OnNext(KeyValuePair<String, Object> value)
     {
-        private static HttpRequestOptionsKey<ISpan> _spanKey = new("Span");
-
-        /// <summary>实例化</summary>
-        public HttpDiagnosticListener() => Name = "HttpHandlerDiagnosticListener";
-
-        /// <summary>下一步</summary>
-        /// <param name="value"></param>
-        public override void OnNext(KeyValuePair<String, Object> value)
+        switch (value.Key)
         {
-            switch (value.Key)
-            {
-                case "System.Net.Http.HttpRequestOut.Start":
+            case "System.Net.Http.HttpRequestOut.Start":
+                {
+                    if (value.Value.GetValue("Request") is HttpRequestMessage request &&
+                        !request.Headers.Contains("traceparent"))
                     {
-                        if (value.Value.GetValue("Request") is HttpRequestMessage request && !request.Headers.Contains("traceparent"))
-                        {
-                            var span = Tracer.NewSpan(request);
-                            request.Options.Set(_spanKey, span);
-                        }
-
-                        break;
+                        var span = Tracer.NewSpan(request);
+                        request.Options.Set(_spanKey, span);
                     }
-                case "System.Net.Http.Exception":
+
+                    break;
+                }
+            case "System.Net.Http.Exception":
+                {
+                    if (value.Value.GetValue("Request") is HttpRequestMessage request &&
+                        request.Options.TryGetValue(_spanKey, out var span))
                     {
-                        if (value.Value.GetValue("Request") is HttpRequestMessage request && request.Options.TryGetValue(_spanKey, out var span))
+                        if (value.Value.GetValue("Exception") is Exception ex)
                         {
-                            if (value.Value.GetValue("Exception") is Exception ex)
+                            span.SetError(ex, null);
+                        }
+                    }
+                    break;
+                }
+
+            case "System.Net.Http.HttpRequestOut.Stop":
+                {
+                    if (value.Value.GetValue("Request") is HttpRequestMessage request &&
+                        request.Options.TryGetValue(_spanKey, out var span))
+                    {
+                        if (value.Value.GetValue("Response") is HttpResponseMessage response)
+                        {
+                            //todo NewLife.Core升级后使用以下这一行
+                            //span.AppendTag(response);
+
+                            // 正常响应，部分作为Tag信息
+                            if (response.StatusCode == HttpStatusCode.OK)
                             {
-                                span.SetError(ex, null);
+                                if (span is DefaultSpan ds && ds.TraceFlag > 0 && (span.Tag.IsNullOrEmpty() || span.Tag.Length < 1024))
+                                {
+                                    // 判断类型和长度
+                                    var content = response.Content;
+                                    var mediaType = content.Headers?.ContentType?.MediaType;
+                                    var len = content.Headers?.ContentLength ?? 0;
+                                    if (len >= 0 && len < 1024 && mediaType.EndsWithIgnoreCase("json", "text"))
+                                    {
+                                        var result = content.ReadAsStringAsync().Result;
+                                        span.Tag = (span.Tag + "\r\n\r\n" + result).Cut(1024);
+                                    }
+                                }
                             }
-                        }
-                        break;
-                    }
-
-                case "System.Net.Http.HttpRequestOut.Stop":
-                    {
-                        if (value.Value.GetValue("Request") is HttpRequestMessage request && request.Options.TryGetValue(_spanKey, out var span))
-                        {
-                            if (value.Value.GetValue("Response") is HttpResponseMessage response && response.StatusCode > (HttpStatusCode)299)
+                            // 异常响应，记录错误
+                            else if (response.StatusCode > (HttpStatusCode)299)
                             {
                                 if (span.Error.IsNullOrEmpty()) span.Error = response.ReasonPhrase;
                             }
-
-                            span.Dispose();
                         }
-                        break;
+
+                        span.Dispose();
                     }
-            }
+                    break;
+                }
         }
     }
 }
