5ecd1ab50bf0d8b958a5fe87c73a89cb78ee2c64
智能大石头 <nnhy@vip.qq.com>
2023-01-01 14:31:11 +0800

    新增安装器工具，用于辅助安装以及修复StarAgent

---
+++ Installer/Command.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Installer;
+
+/// <summary>命令选项</summary>
+internal class Command
+{
+    #region 属性
+    /// <summary>操作名</summary>
+    public String Name { get; set; }
+
+    /// <summary>参数集合</summary>
+    public IList<String> Arguments { get; set; } = new List<String>();
+    #endregion
+
+    #region 方法
+    /// <summary>分析输入参数数组，得到命令选项列表</summary>
+    /// <param name="args"></param>
+    /// <returns></returns>
+    public static IList<Command> Parse(String[] args)
+    {
+        var commands = new List<Command>();
+        if (args == null || args.Length == 0) return commands;
+
+        Command command = null;
+        for (var i = 0; i < args.Length; i++)
+        {
+            // 命令以-开头，其它是参数
+            var arg = args[i];
+            if (arg[0] == '-')
+                commands.Add(command = new Command { Name = arg });
+            else
+            {
+                // 如果第一个参数不是-开头，则添加空名称命令
+                if (command == null)
+                    commands.Add(command = new Command { Name = "" });
+
+                command.Arguments.Add(arg);
+            }
+        }
+
+        return commands;
+    }
+    #endregion
+}
---
+++ Installer/Program.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+
+namespace Installer;
+
+internal class Program
+{
+    static void Main(String[] args)
+    {
+        if (args != null)
+            Console.WriteLine("args: {0}", String.Join(' ', args));
+
+        // 分解命令（可能包括多组），逐个执行
+        // 如：installer.exe -install StarAgent -start StarAgent
+        foreach (var cmd in Command.Parse(args))
+        {
+            switch (cmd.Name.ToLower())
+            {
+                case "-install": Install(cmd.Arguments); break;
+                case "-uninstall": Uninstall(cmd.Arguments); break;
+                case "-reinstall": Reinstall(cmd.Arguments); break;
+                case "-start": Start(cmd.Arguments); break;
+                case "-stop": Stop(cmd.Arguments); break;
+                case "-restart": Restart(cmd.Arguments); break;
+                default:
+                    break;
+            }
+        }
+
+        Console.WriteLine("Hello, World!");
+    }
+
+    static void Install(IList<String> args)
+    {
+
+    }
+
+    static void Uninstall(IList<String> args)
+    {
+
+    }
+
+    static void Reinstall(IList<String> args)
+    {
+
+    }
+
+    static void Start(IList<String> args)
+    {
+        if (args.Count == 0) return;
+
+        Process.Start("net", $"start {args[0]}");
+    }
+
+    static void Stop(IList<String> args)
+    {
+        if (args.Count == 0) return;
+
+        Process.Start("net", $"stop {args[0]}");
+    }
+
+    static void Restart(IList<String> args)
+    {
+        if (args.Count == 0) return;
+
+        Process.Start("net", $"start {args[0]}");
+        Process.Start("net", $"stop {args[0]}");
+    }
+}
