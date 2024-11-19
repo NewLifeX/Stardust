dc750b3fa334088e85536c41ceb0f072dfb3006b
智能大石头 <nnhy@vip.qq.com>
2024-11-20 00:19:29 +0800

    127.0.

--- Stardust.Extensions/TracerMiddleware.cs
+++ Stardust.Extensions/TracerMiddleware.cs
@@ -229,7 +229,7 @@ public class TracerMiddleware
         if (!ss.Contains(baseAddress))
         {
             // 过滤掉本机地址
-            ss = ss.Where(e => !e.EqualIgnoreCase("127.0.0.1", "localhost", "[::1]")).ToList();
+            ss = ss.Where(e => !e.EqualIgnoreCase("127.0.0.1", "localhost", "[::1]") && !e.StartsWith("127.0.")).ToList();
 
             ss.Insert(0, baseAddress);
             set.ServiceAddress = ss.Take(5).Join(",");
