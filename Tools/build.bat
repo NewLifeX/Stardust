0c871bb6508759d8950daf46307b52e1a124eadc
智能大石头 <nnhy@vip.qq.com>
2024-12-20 00:56:53 +0800

    升级更新自动打包脚本以及工具

--- Tools/build.bat
+++ Tools/build.bat
@@ -13,13 +13,13 @@ dotnet publish ..\Stardust.Web\Stardust.Web.csproj --nologo -c Release -o %out%
 del /f/s/q %out%\*.pdb %out%\*.xml %out%\*.deps.json
 rmdir /s/q %out%\wwwroot
 
-clover70.exe zip %base%\StarWeb.zip %out%\
+clover80.exe zip %base%\StarWeb.zip %out%\
 del %out%\*.exe %out%\*.dll %out%\*.config /f/s/q
 move %base%\StarWeb.zip %out%\StarWeb.zip
 
 set out=%base%\agent
 rmdir /s/q %out%
-dotnet publish ..\StarAgent\StarAgent.csproj --nologo -c Release -f net7.0 -o %out%
+dotnet publish ..\StarAgent\StarAgent.csproj --nologo -c Release -f net9.0 -o %out%
 del /f/s/q %out%\*.pdb %out%\*.xml %out%\*.deps.json
 
 mkdir %out%\Config
@@ -29,6 +29,9 @@ xcopy Resources\*.bat %base%\ /y/s
 xcopy Resources\*.sh %base%\ /y/s
 copy clover40.exe %base%\clover40.exe /y
 
+mkdir %base%\Plugins
+xcopy Resources\Plugins\*.dll %base%\Plugins\ /y/s
+
 del %base%\..\star.zip /f
-clover70.exe zip %base%\..\star.zip %base%\
+clover80.exe zip %base%\..\star.zip %base%\
 move %base%\..\star.zip %base%\
