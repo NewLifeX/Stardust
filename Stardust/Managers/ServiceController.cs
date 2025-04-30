b5191438b61a63526d2b138eb8201c956c3ec898
智能大石头 <nnhy@vip.qq.com>
2025-04-30 12:21:40 +0800

    StarAgent拉起应用服务时，通过环境变量设置StarAppId，解决发布名与实际应用名不一致的问题

--- Stardust/Managers/ServiceController.cs
+++ Stardust/Managers/ServiceController.cs
@@ -143,6 +143,8 @@ public class ServiceController : DisposeBase
 
                 // 在环境变量中设置BasePath，不用担心影响当前进程，因为PathHelper仅读取一次
                 //Environment.SetEnvironmentVariable("BasePath", workDir);
+                if (DeployInfo != null && !DeployInfo.Name.IsNullOrEmpty())
+                    Environment.SetEnvironmentVariable("StarAppId", DeployInfo.Name);
 
                 // 工作模式
                 switch (service.Mode)
