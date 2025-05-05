f3a84b1a0c7044a7972e3ae8677b4c05c1b58e31
智能大石头 <nnhy@vip.qq.com>
2025-05-05 23:53:17 +0800

    改进StarAgent打包

--- Tools/pack-agent.bat
+++ Tools/pack-agent.bat
@@ -1,64 +1,53 @@
+@echo off
+
 set base=..\Bin\Agent
+set clover=..\..\..\Tools\clover80.exe
 
 del %base%\*.zip /f/q
 del %base%\*.tar.gz /f/q
 
 pushd %base%\net9.0
 set name=staragent90
-bash -c "tar -czf %name%.tar.gz -- *.exe *.dll *.runtimeconfig.json"
-move %name%.tar.gz ..\
-bash -c "zip -9 %name%.zip -- *.exe *.dll *.runtimeconfig.json"
-move %name%.zip ..\
+%clover% tar ..\%name%.tar.gz *.exe *.dll *.runtimeconfig.json
+%clover% zip ..\%name%.zip *.exe *.dll *.runtimeconfig.json
 popd
 
 pushd %base%\net8.0
 set name=staragent80
-bash -c "tar -czf %name%.tar.gz -- *.exe *.dll *.runtimeconfig.json"
-move %name%.tar.gz ..\
-bash -c "zip -9 %name%.zip -- *.exe *.dll *.runtimeconfig.json"
-move %name%.zip ..\
+%clover% tar ..\%name%.tar.gz *.exe *.dll *.runtimeconfig.json
+%clover% zip ..\%name%.zip *.exe *.dll *.runtimeconfig.json
 popd
 
 pushd %base%\net7.0
 set name=staragent70
-bash -c "tar -czf %name%.tar.gz -- *.exe *.dll *.runtimeconfig.json"
-move %name%.tar.gz ..\
-bash -c "zip -9 %name%.zip -- *.exe *.dll *.runtimeconfig.json"
-move %name%.zip ..\
+%clover% tar ..\%name%.tar.gz *.exe *.dll *.runtimeconfig.json
+%clover% zip ..\%name%.zip *.exe *.dll *.runtimeconfig.json
 popd
 
 pushd %base%\net6.0
 set name=staragent60
-bash -c "tar -czf %name%.tar.gz -- *.exe *.dll *.runtimeconfig.json"
-move %name%.tar.gz ..\
-bash -c "zip -9 %name%.zip -- *.exe *.dll *.runtimeconfig.json"
-move %name%.zip ..\
+%clover% tar ..\%name%.tar.gz *.exe *.dll *.runtimeconfig.json
+%clover% zip ..\%name%.zip *.exe *.dll *.runtimeconfig.json
 popd
 
 pushd %base%\net5.0
 set name=staragent50
-bash -c "tar -czf %name%.tar.gz -- *.exe *.dll *.runtimeconfig.json"
-move %name%.tar.gz ..\
-bash -c "zip -9 %name%.zip -- *.exe *.dll *.runtimeconfig.json"
-move %name%.zip ..\
+%clover% tar ..\%name%.tar.gz *.exe *.dll *.runtimeconfig.json
+%clover% zip ..\%name%.zip *.exe *.dll *.runtimeconfig.json
 popd
 
 pushd %base%\netcoreapp3.1
 set name=staragent31
-bash -c "tar -czf %name%.tar.gz -- *.exe *.dll *.runtimeconfig.json"
-move %name%.tar.gz ..\
-bash -c "zip -9 %name%.zip -- *.exe *.dll *.runtimeconfig.json"
-move %name%.zip ..\
+%clover% tar ..\%name%.tar.gz *.exe *.dll *.runtimeconfig.json
+%clover% zip ..\%name%.zip *.exe *.dll *.runtimeconfig.json
 popd
 
 pushd %base%\net461
 set name=staragent461
-bash -c "zip -9 %name%.zip -- *.exe *.dll StarAgent.exe.config"
-move %name%.zip ..\
+%clover% zip ..\%name%.zip *.exe *.dll StarAgent.exe.config
 popd
 
 pushd %base%\net45
 set name=staragent45
-bash -c "zip -9 %name%.zip -- *.exe *.dll StarAgent.exe.config"
-move %name%.zip ..\
+%clover% zip ..\%name%.zip *.exe *.dll StarAgent.exe.config
 popd
