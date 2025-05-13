979118fa306403cc1aa788283546bd6799e99e28
智能大石头 <nnhy@vip.qq.com>
2025-05-13 15:20:34 +0800

    可能存放clover的多个目录

--- Tools/pack.bat
+++ Tools/pack.bat
@@ -2,15 +2,16 @@
 
 set name=StarWeb
 set clover=..\..\Tools\clover.exe
+if not exist "%clover%" (
+    set clover=..\..\Doc\clover.exe
+)
 
-rem 遍历当前目录中的exe文件
 for %%f in (*.exe) do (
     rem 获取文件名（去掉扩展名）
     set "name=%%~nf"
     goto :found
 )
 
-rem 如果找到了exe文件，打印文件名
 :found
 if defined name (
     del %name%.zip /f/q
