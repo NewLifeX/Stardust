bf07a3ce4b7e7fc032d02684141a69d91d438b56
智能大石头 <nnhy@vip.qq.com>
2025-06-11 21:00:44 +0800

    上传应用zip包时，对于标准包和完整包，自动添加nginx配置文件

--- Stardust.Web/Services/DeployService.cs
+++ Stardust.Web/Services/DeployService.cs
@@ -5,6 +5,7 @@ using NewLife.Net;
 using NewLife.Serialization;
 using Stardust.Data.Deployment;
 using Stardust.Deployment;
+using Stardust.Models;
 using Attachment = NewLife.Cube.Entity.Attachment;
 
 namespace Stardust.Web.Services;
@@ -124,47 +125,82 @@ public class DeployService
         if (!fi.Exists) return;
 
         // 读取其中的nginx文件，识别监听端口
+        var hasNginx = false;
         if (deploy.Port == 0 || deploy.Urls.IsNullOrEmpty())
         {
             // 在zip包中查找后缀为.nginx或.conf的文件，以文本打开，按照nginx文件格式识别其中的listen监听端口
             using var zip = ZipFile.Open(fi.FullName, ZipArchiveMode.Read);
             foreach (var entry in zip.Entries)
             {
-                if (entry.FullName.EndsWithIgnoreCase(".nginx") || entry.FullName.EndsWithIgnoreCase(".conf"))
+                if (!entry.Name.EndsWithIgnoreCase(".nginx", ".conf")) continue;
+
+                var nginx = new NginxFile();
+                if (!nginx.Parse(entry.Open().ToStr())) continue;
+
+                // 获取后端端口
+                if (deploy.Port == 0)
                 {
-                    var nginx = new NginxFile();
-                    if (nginx.Parse(entry.Open().ToStr()))
+                    var backend = nginx.GetBackends().FirstOrDefault();
+                    if (!backend.IsNullOrEmpty())
                     {
-                        // 获取后端端口
-                        if (deploy.Port == 0)
-                        {
-                            var backend = nginx.GetBackends().FirstOrDefault();
-                            if (!backend.IsNullOrEmpty())
-                            {
-                                var uri = new Uri(backend);
-                                if (uri.Port > 0) deploy.Port = uri.Port;
-                            }
-                        }
-
-                        // 获取对外服务地址
-                        if (deploy.Urls.IsNullOrEmpty() && !nginx.ServerName.IsNullOrEmpty())
-                        {
-                            var schema = nginx.Ports.Any(e => e % 443 == 0) ? "https" : "http";
-                            var host = nginx.ServerName.Split(',').FirstOrDefault();
-                            var port = nginx.Ports.Count > 0 ? nginx.Ports.Max() : 0;
-
-                            if (schema == "https" && port % 443 == 0 || schema == "http" && port % 80 == 0)
-                                deploy.Urls = $"{schema}://{host}";
-                            else
-                                deploy.Urls = $"{schema}://{host}:{port}";
-                        }
-
-                        //deploy.Update();
-
-                        break; // 找到一个就行了
+                        var uri = new Uri(backend);
+                        if (uri.Port > 0) deploy.Port = uri.Port;
                     }
                 }
+
+                // 获取对外服务地址
+                if (deploy.Urls.IsNullOrEmpty() && !nginx.ServerName.IsNullOrEmpty())
+                {
+                    var schema = nginx.Ports.Any(e => e % 1000 == 443) ? "https" : "http";
+                    var host = nginx.ServerName.Split(',').FirstOrDefault();
+                    var port = nginx.Ports.Count > 0 ? nginx.Ports.Max() : 0;
+
+                    if (schema == "https" && port % 1000 == 443 || schema == "http" && port % 100 == 80)
+                        deploy.Urls = $"{schema}://{host}";
+                    else
+                        deploy.Urls = $"{schema}://{host}:{port}";
+                }
+
+                hasNginx = true;
+                //deploy.Update();
+
+                break; // 找到一个就行了
             }
+        }
+
+        // 如果是标准包或者完整包，检测zip包是否有nginx配置文件，如果没有则主动添加一个
+        if (!hasNginx && version.Mode is DeployModes.Standard or DeployModes.Full && deploy.Port > 0 && !deploy.Urls.IsNullOrEmpty())
+        {
+            // 如果没有nginx配置文件，则添加一个默认的
+            using var zip = ZipFile.Open(fi.FullName, ZipArchiveMode.Update);
+            if (!zip.Entries.Any(e => e.Name.EndsWithIgnoreCase(".nginx", ".conf")))
+            {
+                var uri = new Uri(deploy.Urls);
+                var nginx = new NginxFile
+                {
+                    ServerName = uri.Host
+                };
+                if (uri.Port % 1000 == 443)
+                    nginx.Ports = [uri.Port / 1000 + 80, uri.Port];
+                else
+                    nginx.Ports = [uri.Port];
+
+                // 后端端口
+                nginx.SetBackends($"http://localhost:{deploy.Port}");
+
+                // 保存到zip包中
+                var entry = zip.CreateEntry($"{uri.Host}.nginx", CompressionLevel.Optimal);
+                using var stream = entry.Open();
+                stream.Write(nginx.ToString().GetBytes());
+            }
+
+            zip.TryDispose();
+
+            // 更新附件信息
+            fi.Refresh();
+            attachment.Hash = fi.MD5().ToHex();
+            attachment.Size = fi.Length;
+            attachment.Update();
         }
     }
 }
