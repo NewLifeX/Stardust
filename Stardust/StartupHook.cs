4e1184fdc838f46cd62a83b31baf06534c2ec5d0
智能大石头 <nnhy@vip.qq.com>
2025-08-06 23:11:44 +0800

    [feat] 增加StartupHook钩子

---
+++ Stardust/StartupHook.cs
@@ -0,0 +1,82 @@
+﻿#if NETCOREAPP
+using System.Reflection;
+using System.Runtime.InteropServices;
+using System.Runtime.Loader;
+using NewLife;
+using NewLife.Log;
+using NewLife.Model;
+using Stardust;
+
+/// <summary>启动钩子</summary>
+internal class StartupHook
+{
+    /// <summary>被 DOTNET_STARTUP_HOOKS 调用的初始化方法</summary>
+    public static void Initialize()
+    {
+        var env = Environment.GetEnvironmentVariable("DOTNET_STARTUP_HOOKS");
+        var hooks = RuntimeInformation.IsOSPlatform(OSPlatform.Windows) ? env?.Split(';') : env?.Split(':');
+        if (hooks == null || hooks.Length == 0) return;
+
+        var file = hooks.FirstOrDefault(e => e.EndsWith("Stardust.dll", StringComparison.OrdinalIgnoreCase));
+        if (String.IsNullOrEmpty(file) || !File.Exists(file)) return;
+
+        var dir = Path.GetDirectoryName(file)!;
+
+        var context = new MyAssemblyLoadContext();
+        AssemblyLoadContext.Default.Resolving += (_, name) =>
+        {
+            var assemblyPath = Path.Combine(dir, name.Name + ".dll");
+            if (!File.Exists(assemblyPath)) return null;
+
+            try
+            {
+                var assemblyName = AssemblyName.GetAssemblyName(assemblyPath);
+                if (name.Version == assemblyName.Version)
+                {
+                    var keyToken = name.GetPublicKeyToken();
+                    var assemblyKeyToken = assemblyName.GetPublicKeyToken();
+                    if (keyToken!.SequenceEqual(assemblyKeyToken!))
+                    {
+                        return name.Name!.StartsWith("NewLife", StringComparison.Ordinal)
+                            ? AssemblyLoadContext.Default.LoadFromAssemblyPath(assemblyPath)
+                            : context.LoadFromAssemblyPath(assemblyPath);
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.ToString());
+            }
+
+            return null;
+        };
+
+        var type = typeof(StartupHook);
+        type.InvokeMember(nameof(Init), BindingFlags.InvokeMethod | BindingFlags.Static | BindingFlags.NonPublic, null, null, [dir]);
+    }
+
+    private static void Init(String dir)
+    {
+        //XTrace.UseConsole();
+
+        var has = Directory.GetFiles(Environment.CurrentDirectory, "*.dll", SearchOption.TopDirectoryOnly)
+            .Any(e => e.StartsWith("NewLife", StringComparison.OrdinalIgnoreCase));
+        if (!has)
+        {
+            var name = Assembly.GetEntryAssembly()?.GetName().Name;
+            Environment.SetEnvironmentVariable("BASEPATH", Path.Combine(dir, name!));
+
+            Runtime.CreateConfigOnMissing = false;
+        }
+
+        var services = ObjectContainer.Current;
+        services.AddStardust();
+    }
+
+    internal class MyAssemblyLoadContext : AssemblyLoadContext
+    {
+        protected override Assembly Load(AssemblyName assemblyName) => null!;
+    }
+}
+
+#endif
