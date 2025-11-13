f4bd747ca209ddd04d655cc18392467c8a315fa2
大石头 <nnhy@vip.qq.com>
2025-11-13 11:15:09 +0800

    v3.5.2025.1113 支持.net10

--- Tools/Resources/linux-service.sh
+++ Tools/Resources/linux-service.sh
@@ -4,7 +4,7 @@
 echo "Install Stardust on Linux"
 
 if [ ! -d "/usr/share/dotnet/" ]; then
-	curl https://x.newlifex.com/dotnet/net8.sh | sudo bash
+	curl https://x.newlifex.com/dotnet/net10.sh | sudo bash
 fi
 
 sudo dotnet agent/StarAgent.dll -install -server http://127.0.0.1:6600
